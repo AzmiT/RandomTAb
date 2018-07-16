a500edc722fcd21b090b7d99290533b3ef685aa2
AZMI TOUIL <touilazmi@gmail.com>
2018-07-16 10:48:50 +0100

    RandomTab

---
+++ src/Table.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Table {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		int nb=(int) (Math.random() * 51);
+		ArrayList <Integer> Tab1 = new ArrayList<Integer>(nb);
+	
+		
+		for (int i = 0; i < nb; i++) {
+			int n1=(int) (Math.random() * 9);
+			Tab1.add(n1);
+			
+               System.out.println(Tab1.get(i));
+	}
+
+
+	
+	}}
+
+	
+         
+		
+       
+
+	
+
+
